3bda65933e655893ee4002fed541cbd409979802
Nikitha Chettiar <nikithauc@gmail.com>
2021-03-01 12:44:18 -0800

    setting an alias

--- scripts/GetPackageVersion.ps1
+++ scripts/GetPackageVersion.ps1
@@ -1,4 +1,4 @@
-$currentVersionString = (Get-Content  $(System.DefaultWorkingDirectory)/_TypeScript Beta Production Build/drop/package.json) -join "`n" | ConvertFrom-Json | Select -ExpandProperty "version"
+$currentVersionString = (Get-Content  $(System.DefaultWorkingDirectory)/typingsBetaBuild/drop/package.json) -join "`n" | ConvertFrom-Json | Select -ExpandProperty "version"
 Write-Host "Package version is '$($currentVersionString)'" -ForegroundColor Green;
 
 Write-Host "##vso[task.setvariable variable=VERSION_STRING]$($currentVersionString)";
