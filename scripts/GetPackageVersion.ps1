5b1ce66863d134632a1c411591cd24e144a4c1d0
Nikitha Chettiar <nikithauc@gmail.com>
2021-03-01 14:18:07 -0800

    Changing the variable for reading source directory

--- scripts/GetPackageVersion.ps1
+++ scripts/GetPackageVersion.ps1
@@ -1,4 +1,4 @@
-$currentVersionString = (Get-Content  $(System.DefaultWorkingDirectory)/typingsBetaBuild/drop/package.json) -join "`n" | ConvertFrom-Json | Select -ExpandProperty "version"
+$currentVersionString = (Get-Content  $env:SYSTEM_DEFAULTWORKINGDIRECTORY/typingsBetaBuild/drop/package.json) -join "`n" | ConvertFrom-Json | Select -ExpandProperty "version"
 Write-Host "Package version is '$($currentVersionString)'" -ForegroundColor Green;
 
 Write-Host "##vso[task.setvariable variable=VERSION_STRING]$($currentVersionString)";
